82d775f8890328fd55ab670b19c0e77f702d0692
Cylix Lee <cylix.lee@outlook.com>
2025-06-27 19:37:52 +0800

    feature: Add some layout code.

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,80 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 24,
             children: [
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                ),
-                child: const Text("Hello World"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 12,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: const Text("Hello World"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: const Text("Hello World"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: const Text("Hello World"),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 12,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: const Text("Hello World"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: const Text("Hello World"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: const Text("Hello World"),
+                  ),
+                ],
               ),
             ],
           ),
         ),
         appBar: AppBar(title: const Text("Flutter Basic Layout")),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.abc), label: "Abc"),
+          ],
+        ),
       ),
       theme: ThemeData.from(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
