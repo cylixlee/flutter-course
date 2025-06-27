d521f5a87bf76fb4cac4baceb966f3332efc4f3f
Cylix Lee <cylix.lee@outlook.com>
2025-06-27 16:57:31 +0800

    feature: Write the official example with `provider`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MainAppModel(),
+      child: const MainApp(),
+    ),
+  );
+}
+
+class MainAppModel with ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+  set counter(int value) {
+    _counter = value;
+    notifyListeners();
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -9,11 +25,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('You have pushed the button this many times:'),
+              Consumer<MainAppModel>(
+                builder: (context, value, _) {
+                  return Text(
+                    '${value.counter}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                },
+              ),
+            ],
+          ),
         ),
+        appBar: AppBar(title: Text('Flutter Demo Home Page')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Provider.of<MainAppModel>(context, listen: false).counter++;
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      ),
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
     );
   }
